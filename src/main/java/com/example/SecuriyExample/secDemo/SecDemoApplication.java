43544667d6c582cd87b2fb8eddbd73fee05717b9
Manish Vishwakarma <mv42324@gmail.com>
2023-06-23 12:47:50 +0530

    Code committed

---
+++ src/main/java/com/example/SecuriyExample/secDemo/SecDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecuriyExample.secDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecDemoApplication.class, args);
+	}
+
+}
